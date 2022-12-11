8fb484c06f98836a4a68d350dac15e2176d9cdc5
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2022-12-11 11:11:23 +0700

    Update Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -77,7 +77,7 @@ export default {
         </form>
 
         <div class="search-under"  v-if="data.length || load==true">
-            <div class="back" v-if="data.length || load" ></div>
+            <div class="back" v-if="data.length > 0 || load" ></div>
             <p class="load" v-if="kiem()">Loading...</p>
             <div v-else class="results">
                 <router-link class="result" v-for="(app) in data" :key="app.id" :to="'/app/' + app.id">
